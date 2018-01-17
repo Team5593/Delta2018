313c6c5b6463c82ae4fa448e9374ec48dc1869a3
liamkinne <kinneliam@gmail.com>
2018-01-17 19:04:45 +1100

    remove bloat stuff

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -18,56 +18,27 @@
 class Robot : public frc::TimedRobot {
 public:
 	void RobotInit() override {
-		m_chooser.AddDefault("Default Auto", &m_defaultAuto);
-		m_chooser.AddObject("My Auto", &m_myAuto);
-		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 	}
 
 	void DisabledInit() override {}
 
 	void DisabledPeriodic() override {
-		frc::Scheduler::GetInstance()->Run();
 	}
 
 	void AutonomousInit() override {
-		std::string autoSelected = frc::SmartDashboard::GetString(
-				"Auto Selector", "Default");
-		if (autoSelected == "My Auto") {
-			m_autonomousCommand = &m_myAuto;
-		} else {
-			m_autonomousCommand = &m_defaultAuto;
-		}
-
-		m_autonomousCommand = m_chooser.GetSelected();
-
-		if (m_autonomousCommand != nullptr) {
-			m_autonomousCommand->Start();
-		}
 	}
 
 	void AutonomousPeriodic() override {
-		frc::Scheduler::GetInstance()->Run();
 	}
 
 	void TeleopInit() override {
-		// Stop auto commands
-		if (m_autonomousCommand != nullptr) {
-			m_autonomousCommand->Cancel();
-			m_autonomousCommand = nullptr;
-		}
 	}
 
-	void TeleopPeriodic() override { frc::Scheduler::GetInstance()->Run(); }
+	void TeleopPeriodic() override {}
 
 	void TestPeriodic() override {}
 
 private:
-	// Have it null by default so that if testing teleop it
-	// doesn't have undefined behavior and potentially crash.
-	frc::Command* m_autonomousCommand = nullptr;
-	ExampleCommand m_defaultAuto;
-	MyAutoCommand m_myAuto;
-	frc::SendableChooser<frc::Command*> m_chooser;
 };
 
 START_ROBOT_CLASS(Robot)
